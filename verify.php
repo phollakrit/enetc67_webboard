ab32071b7bc3bc10866da66058f31e684f82092c
phollakrit <66252619+phollakrit@users.noreply.github.com>
2024-08-13 12:40:23 +0700

    work6
    work6

--- verify.php
+++ verify.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+if(isset($_SESSION['id'])){
+    header("location:index.php");
+    die();
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,10 +17,16 @@
 	<center>
 	<?php 
 		if ($_POST["login"]=="admin" && $_POST["pwd"]=="ad1234") {
+			$_SESSION['username']="admin";
+			$_SESSION['role']="a";
+			$_SESSION['id']=session_id();
 			echo "ยินดีต้อนรับคุณ ADMIN <br>";
 			echo "<a href="."index.php".">กลับไปยังหน้าหลัก</a>";
 		}
 		elseif ($_POST["login"]=="member" && $_POST["pwd"]=="mem1234"){
+			$_SESSION['username']="member";
+			$_SESSION['role']="m";
+			$_SESSION['id']=session_id();
 			echo "ยินดีต้อนรับคุณ MEMBER <br>";
 			echo "<a href="."index.php".">กลับไปยังหน้าหลัก</a>";
 		}
